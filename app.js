fafc09183c89e2769cdf796aef637192ce8a2ba0
hamzaihsa15 <71670698+hamzaihsa15@users.noreply.github.com>
2021-01-07 13:57:06 +0500

    Update app.js

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 const db =
-  "mongodb+srv://ali:123@cluster0.gcvge.mongodb.net/cluster0?retryWrites=true&w=majority";
+  "mongodb+srv://hamza:1234@cluster0.kdzfc.mongodb.net/cluster0?retryWrites=true&w=majority";
 
 mongoose
   .connect(db, {
